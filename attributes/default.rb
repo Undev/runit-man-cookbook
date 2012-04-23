2c9f8dfd0b2630d27ab455f4ae138105dec85351
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2012-04-23 13:56:45 +0400

    auth was replaced with users.

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@
 set[:runit_man][:version] = "latest"
 set[:runit_man][:use_multiuser_rvm] = false
 set[:runit_man][:port] = 14500
-set[:runit_man][:auth] = [] # set to ["username:password", ...] to use http auth.
+set[:runit_man][:users] = [] # set to ["username:password", ...] to use http auth.
 set[:runit_man][:mode] = "rw" # set to "ro" to run in readonly mode.
 set[:runit_man][:logger] = "svlogd" # can be replaced with logger:/var/log:info
 set[:runit_man][:files] = []
